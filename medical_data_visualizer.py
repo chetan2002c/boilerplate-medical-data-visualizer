4f3340efa4b0f887cf36ecec1706fcdaa400e2fc
chetan2002c <125239178+chetan2002c@users.noreply.github.com>
2025-05-27 21:19:20 +0530

    medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -20,31 +20,24 @@ def draw_cat_plot():
                      id_vars=['cardio'],
                      value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
-
     # 6
     df_cat = df_cat.groupby(['cardio', 'variable', 'value'], as_index=False).size()
-    df_cat.rename(columns={'size': 'total'}, inplace=True)
-
-    
+    df_cat.rename(columns={'size': 'total'}, inplace=True) 
 
     # 7
     plot = sns.catplot(data=df_cat,kind='bar',
                 x='variable',y='total',
                 hue='value',
-                col='cardio'
-    )
-
-
+                col='cardio')
+    
     # 8
     fig = plot.fig
-
 
     # 9
     fig.savefig('catplot.png')
     return fig
 
-
-10
+# 10
 def draw_heat_map():
     # 11
     df_heat = df[
@@ -54,15 +47,12 @@ def draw_heat_map():
         (df['weight'] >= df['weight'].quantile(0.025))&
         (df['weight'] <= df['weight'].quantile(0.975))
         ]
-
     # 12
     corr = df_heat.corr()
 
     # 13
     mask = np.triu(corr)
-
-
-
+    
     # 14
     fig, ax = plt.subplots(figsize=(10, 8))
 
